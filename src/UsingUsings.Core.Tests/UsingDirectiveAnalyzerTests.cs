8674db60f3383c5e1c49a7144e4c42f27910fcea
Jason Bock <jason.r.bock@outlook.com>
2021-11-08 11:37:40 -0600

    Initial frame/core/bits/setup/etc.

---
+++ src/UsingUsings.Core.Tests/UsingDirectiveAnalyzerTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace UsingUsings.Core.Tests
+{
+	public static class UsingDirectiveAnalyzerTests
+	{
+		[Test]
+		public static async Task AnalyzeAsync() => await Task.CompletedTask.ConfigureAwait(false);
+	}
+}
---
+++ src/UsingUsings.Core/UsingDirectiveAnalyzer.cs
@@ -0,0 +1,7 @@
+﻿namespace UsingUsings.Core
+{
+	public static class UsingDirectiveAnalyzer
+	{
+		public static async Task AnalyzeAsync() => await Task.CompletedTask.ConfigureAwait(false);
+	}
+}
